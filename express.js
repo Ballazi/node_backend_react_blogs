c48970ceb81760ed271c9bc41e7874bad198fb5f
Ballazi <balram17dec1999@gmail.com>
2021-12-31 14:00:26 +0530

    Hi...

--- express.js
+++ express.js
@@ -5,13 +5,23 @@ const homerouter = require("./router/homeRouter");
 const writerrouter = require("./router/writerRouter");
 const singlepostRouter = require("./router/singlepostRouter");
 const cors = require("cors");
+const path = require("path");
 app.use(cors());
 app.use(express.json());
+
+
+app.use(express.static(path.join(__dirname, "/blogreact/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/blogreact/build', 'index.html'));
+});
 
 
 app.use("/api/v1/common",commonrouter);
 app.use("/api/v1/home",homerouter);
 app.use("/api/v1/writer",writerrouter);
 app.use("/api/v1/singlepost",singlepostRouter);
+
+
 
 module.exports = app;
